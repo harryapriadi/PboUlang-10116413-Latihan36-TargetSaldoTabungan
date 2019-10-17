96001f70faded67ee8f8226e83025eb6a438a14f
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-17 14:13:45 +0700

    PboUlang-10116413-Latihan36-TargetSaldoTabungan

---
+++ src/pboulang/pkg10116413/latihan36/targetsaldotabungan/PboUlang10116413Latihan36TargetSaldoTabungan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan36.targetsaldotabungan;
+
+/**
+ *
+ * @author Lenovo
+ * NAMA         : HARRY APRIADI
+ * KELAS        : PBOULANG
+ * NIM          : 10116413
+ * DESKRIPSI    : program ini berisi program yang menampilkan target saldo
+ * tabungan nasabah
+ */
+public class PboUlang10116413Latihan36TargetSaldoTabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tabungan tab = new Tabungan();
+
+        tab.saldo = 3500000;
+        tab.bunga = 8;
+        int saldoTarget = 6000000;
+
+        tab.HitungLamaSaldoTarget(tab.saldo, saldoTarget);
+    }
+
+}
